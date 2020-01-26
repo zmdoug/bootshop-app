2a598cfc8e51e487346e3551de1b3b8811c78b53
Douglas Maehler <zmdoug@gmail.com>
2020-01-26 01:48:41 -0300

    add to cart redirect

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,6 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import { addToCartSuccess, updateAmountSuccess } from './actions';
+import history from '../../../services/history';
 import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
@@ -34,6 +35,8 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+
+    history.push('/cart');
   }
 }
 
